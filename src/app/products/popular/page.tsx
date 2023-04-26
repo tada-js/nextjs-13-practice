f08be1a5de16c438f40c1ea18a8d3c5a179df6c9
tada-js <justfix010@gmail.com>
2023-04-27 04:38:24 +0900

    Add : products 인기 상품 페이지

---
+++ src/app/products/popular/page.tsx
@@ -0,0 +1,11 @@
+import ProductsNavigationButton from '@/components/ProductsNavigationButton';
+
+const PopularPage = () => {
+  return (
+    <>
+      <h2>인기 상품 준비 중..</h2>
+      <ProductsNavigationButton />
+    </>
+  );
+};
+export default PopularPage;

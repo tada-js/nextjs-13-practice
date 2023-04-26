afe2c58a142c0980026effe9235ff55259acefc6
tada-js <justfix010@gmail.com>
2023-04-27 03:05:54 +0900

    Feat : 메인페이지, products페이지 not found 추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,4 @@
+const NotFoundPage = () => {
+  return <h1>페이지를 찾을 수 없습니다.😭</h1>;
+};
+export default NotFoundPage;
---
+++ src/app/products/not-found.tsx
@@ -0,0 +1,4 @@
+const ProductNotFound = () => {
+  return <h1>해당 제품을 찾을 수 없습니다.❌</h1>;
+};
+export default ProductNotFound;

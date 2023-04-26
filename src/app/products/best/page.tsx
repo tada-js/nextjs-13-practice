22a76b005e6f030d735ffebb2c3f69a46cabaf12
tada-js <justfix010@gmail.com>
2023-04-27 04:38:13 +0900

    Add : products 베스트 상품 페이지

---
+++ src/app/products/best/page.tsx
@@ -0,0 +1,11 @@
+import ProductsNavigationButton from '@/components/ProductsNavigationButton';
+
+const BestItemPage = () => {
+  return (
+    <>
+      <h2>베스트 상품 준비 중..</h2>
+      <ProductsNavigationButton />
+    </>
+  );
+};
+export default BestItemPage;
